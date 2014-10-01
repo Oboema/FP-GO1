5ac9a39c412a36290b51cb69b626914909cbc012
jterbraak <joostterbraak@gmail.com>
2014-10-01 12:56:50 +0200

    Token definition
    Defining the Tokens and the TokenTree used for communicating between the
    parser and the emitter

---
+++ Grammar.hs
@@ -0,0 +1,33 @@
+
+data TokenType	=Plus
+				|Min
+				|Mul
+				|Div
+				|OpBool
+				|Statement
+				|DefVar
+				|Assignment
+				|If
+				|Then
+				|Else
+				|While
+				|For
+				|Number
+				|Var
+				|Nop
+				
+
+type Token	=(TokenType,[Char])
+
+TokenTree 	=TokenLeaf Token
+			|TokenNode Token TokenTree TokenTree
+			
+			
+if	=TokenTree (If,"if") 
+	(TokenTree (OpBool,"==") 
+		(TokenLeaf (Number, "3")) 
+		(TokenLeaf (Var, "c")))
+	
+	(TokenTree (Then,"then") 
+		(TokenNode (Statement, "" ) (TokenLeaf (Nop,"") (TokenLeaf (Nop,"")))) --thentree
+		(TokenNode (Statement, "" ) (TokenLeaf (Nop,"") (TokenLeaf (Nop,""))))) --elsetree
